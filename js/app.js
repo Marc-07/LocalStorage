c3bb516301d4e01fb7810c602ecd4bec416ce546
Marc-07 <maanraca01@gmail.com>
2023-04-06 15:49:21 -0500

    Se agrega mensaje de alerta

--- js/app.js
+++ js/app.js
@@ -16,5 +16,33 @@ function evenListeners(){
 function agregarTweet(e){
     e.preventDefault();
 
-    console.log('agregando tweet...')
+    //textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    //Validacion 
+
+    if(tweet === ''){
+        mostrarError('Un mensaje no puede ir vacio');
+        return;
+    }
+
+}
+
+//Mostrar mensaje de error
+
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //Insertarlo en el contenido
+
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    //Elimina la alerta despues de 3'
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+
 }
